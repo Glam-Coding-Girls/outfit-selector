cfe4a1b7c77fa1871dc45d582ef3ebe27ee839fa
kikibres <kristinabressler@yahoo.com>
2019-12-17 17:15:14 -0500

    kiki edits

--- client/src/components/MyOutfits.js
+++ client/src/components/MyOutfits.js
@@ -89,7 +89,7 @@ export class MyOutfits extends Component {
     return this.props.myOutfits.map((outfit,ind)=>{
        return (
          <div key={ind} className="outfit-display">
-               <button className="delete" onClick={()=>this.deleteSelected(outfit)}>Del</button>
+               <button className="delete" onClick={()=>this.deleteSelected(outfit)}><i class="fas fa-times-circle"></i></button>
             {this.displayMyClothes(outfit.selectedClothes)}
             <button onClick={()=>this.sharePics(outfit)} className="btn btn-primary">Share</button>
          </div>
